a4e1b88a69a3fbdf03872be478cae813013bace0
unknown <juanjo_guevara@hotmail.com>
2013-09-10 11:09:02 -0600

    spec for number 1

---
+++ kata.specs/when_calculating_value_for_1.cs
@@ -0,0 +1,15 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_calculating_value_for_1 : given_a_fizzbuzz_controller
+    {
+        Because of =
+            () => _result = _fizzBuzzer.Calculate(1);
+
+        It should_do_something =
+            () => _result.ShouldEqual("1");
+
+        static string _result;
+    }
+}
--- kata/FizzBuzzer.cs
+++ kata/FizzBuzzer.cs
@@ -2,9 +2,21 @@
 {
     public class FizzBuzzer
     {
+        //dice javier que esta funcioncita es fea :(
+        public string CalculateAll()
+        {
+            var returnValue = "";
+            for (int i = 1; i <= 100; i++)
+               returnValue += Calculate(i);
+            return returnValue;
+        }
+
         public string Calculate(int i)
         {
-            return "Fizz";
+            if (i.Equals(3))
+                return "Fizz";
+
+            return "Buzz";
         }
     }
 }
