ec6ab6b38e057e0c2f673db0bd5a671529246c07
Rene A. Rosa <realpasro09@hotmail.com>
2013-09-10 08:25:43 -0600

    adding spec when_send_1

---
+++ kata.specs/when_send_1.cs
@@ -0,0 +1,19 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_send_1
+    {
+        static FizzBuzz _fizzBuzz;
+        static string _result;
+
+        Establish context =
+            () => { _fizzBuzz = new FizzBuzz(); };
+
+        Because of =
+            () => _result = _fizzBuzz.Send("1");
+
+        It should_return_1 =
+            () => _result.ShouldEqual("1");
+    }
+}
---
+++ kata/FizzBuzz.cs
@@ -0,0 +1,10 @@
+﻿namespace kata
+{
+    public class FizzBuzz
+    {
+        public string Send(string values)
+        {
+            return "0";
+        }
+    }
+}
