92d8edde57cfda8b026385e389e9004998fac598
Rene A. Rosa <realpasro09@hotmail.com>
2013-09-11 09:45:11 -0600

    adding spec when_sending_5

--- kata.specs/when_sending_5.cs
+++ kata.specs/when_sending_5.cs
@@ -2,14 +2,14 @@ using Machine.Specifications;
 
 namespace kata.specs
 {
-    public class when_sending_4 : given_a_fizz_buzz_context
+    public class when_sending_5 : given_a_fizz_buzz_context
     {
         static string _result;
 
         Because of =
-            () => _result = _fizzBuzz.Send(4);
+            () => _result = _fizzBuzz.Send(5);
 
-        It should_return_4 =
-            () => _result.ShouldEqual("4");
+        It should_return_buzz =
+            () => _result.ShouldEqual("Buzz");
     }
 }
--- kata/FizzBuzz.cs
+++ kata/FizzBuzz.cs
@@ -5,11 +5,7 @@
         public string Send(int value)
         {
             if (value == 3) return "Fizz";
-            if (value == 2)
-            {
-                return "2";
-            }
-            return "1";
+            return value.ToString();
         }
     }
 }
