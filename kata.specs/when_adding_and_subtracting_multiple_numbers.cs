0973e82d14814afaccedbb801e02ac8a03cd7b66
Byron Sommardahl <byron@acklenavenue.com>
2013-10-04 10:07:20 -0500

    Ping

---
+++ kata.specs/when_adding_and_subtracting_multiple_numbers.cs
@@ -0,0 +1,15 @@
+using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_and_subtracting_multiple_numbers : given_a_string_calculator_context
+    {
+        static int _result;
+
+        Because of =
+            () => _result = _calculator.Calculate("10+20-5+4-1");
+
+        It should_return_the_sum =
+            () => _result.ShouldEqual(8);
+    }
+}
--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -7,20 +7,26 @@ namespace kata
     {
         public int Calculate(string s)
         {
-            if (s == "10-3-4")
-            {
+            if (s == "10+20-5+4-1")
                 return 0;
-            }
-            if(s=="10-5")
+
+            if (s.Contains("-"))
             {
-                return 5;
-            }
-            if (s == "3-2")
-            {
-                return 1;
-            }
+                return Restar(s);
+            }            
             var numbers = s.Split('+');
             return numbers.Sum(x => Convert.ToInt32(x)); ;
+        }
+
+        static int Restar(string s)
+        {
+            var numbersToSubtract = s.Split('-');
+            var sumOfNegatives = Convert.ToInt32(numbersToSubtract.First());
+            for (int i = 1; i < numbersToSubtract.Length; i++)
+            {
+                sumOfNegatives -= Convert.ToInt32(numbersToSubtract[i]);
+            }
+            return sumOfNegatives;
         }
     }
 }
