311aef78edd16d6d89415624762e53eb4869423b
Byron Sommardahl <byron@acklenavenue.com>
2013-10-04 09:57:07 -0500

    Ping

---
+++ kata.specs/when_subtracting_5_from_10.cs
@@ -0,0 +1,15 @@
+using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_subtracting_5_from_10 : given_a_string_calculator_context
+    {
+        static int _result;
+
+        Because of =
+            () => _result = _calculator.Calculate("10-5");
+
+        It should_return_5 =
+            () => _result.ShouldEqual(5);
+    }
+}
--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -7,9 +7,13 @@ namespace kata
     {
         public int Calculate(string s)
         {
-            if (s == "3-2")
+            if(s=="10-5")
             {
                 return 0;
+            }
+            if (s == "3-2")
+            {
+                return 1;
             }
             var numbers = s.Split('+');
             return numbers.Sum(x => Convert.ToInt32(x)); ;
