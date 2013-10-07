4f148df143c0c7974bd1af71adef5e9ebc6d5f57
Rene A. Rosa <realpasro09@hotmail.com>
2013-10-07 09:19:09 -0600

    adding a spec

---
+++ kata.specs/when_sum_and_subtracting_2_3_5_2.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_sum_and_subtracting_2_3_5_2: given_a_string_calculator_context
+    {
+        Because of =
+            () => _result = StringCalculator.Calculate("2+3+5-2");
+
+        It should_return_8 =
+            () => _result.ShouldEqual(8);
+    }
+}
--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace kata
 {
@@ -6,16 +7,28 @@ namespace kata
     {
         public int Calculate(string s)
         {
+            if (s == "2+3+5-2")
+            {
+                return 0;
+            }
             if (s.Length == 0) return 0;
-            if (s.Contains("2-1")) return -1;
-            int result = 0;            
+            if (s.Contains("-"))
+            {
+                
+                string[] subtractValues = s.Split('-');
+                var total = Convert.ToInt32(subtractValues[0]);
+
+                for (int i = 1; i < subtractValues.Length; i++)
+                {
+                    total -= Convert.ToInt32(subtractValues[i]);    
+                    
+                }
+                return total;     
+
+            }
             string[] values = s.Split('+');
 
-            foreach (var value in values)
-            {
-                result += Convert.ToInt32(value);
-            }
-            return result;                
+            return values.Sum(value => Convert.ToInt32(value));                
         }
     }
 }
