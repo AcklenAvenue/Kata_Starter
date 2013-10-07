b841c1ad27d462239d4d3eecbfca6504f6dee3e0
unknown <juanjo_guevara@hotmail.com>
2013-10-07 09:49:59 -0600

    added test for adding multiple numbers

---
+++ kata.specs/when_adding_multiple_numbers.cs
@@ -0,0 +1,19 @@
+using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_multiple_numbers
+    {
+        Establish context =
+            () => { _calculator = new StringCalculator(); };
+
+        Because of =
+            () => _result = _calculator.Calculate("2+1+3");
+
+        It should_do_something =
+            () => _result.ShouldEqual(6);
+
+        static StringCalculator _calculator;
+        static int _result;
+    }
+}
