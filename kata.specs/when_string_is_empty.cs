ab585afbe413a962fe8fda925c97ad1b7831b1e5
rsiwady29 <rsiwady29@gmail.com>
2013-10-07 08:55:05 -0600

    add 2 spec

---
+++ kata.specs/when_string_is_empty.cs
@@ -0,0 +1,19 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_string_is_empty
+    {
+        static StringCalculator _stringCalculator;
+        static int _result;
+
+        Establish context =
+            () => { _stringCalculator = new StringCalculator(); };
+
+        Because of =
+            () => _result = _stringCalculator.Calculate("");
+
+        It should_do_something =
+            () => _result.ShouldEqual(0);
+    }
+}
--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -1,10 +1,17 @@
-﻿namespace kata
+﻿using System;
+
+namespace kata
 {
     public class StringCalculator
     {
         public int Calculate(string s)
-        {
-            return 0;
+        {          
+            if (s.Length > 0)
+            {
+                string[] values = s.Split('+');
+                return Convert.ToInt32(values[0]) + Convert.ToInt32(values[1]);                
+            }
+            return -1;
         }
     }
 }
