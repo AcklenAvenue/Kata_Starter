a485076311ce0f515c6ee640fea6c83803979544
Rene A. Rosa <realpasro09@hotmail.com>
2013-10-07 08:49:42 -0600

    adding first spec

---
+++ kata.specs/when_sum_1_1.cs
@@ -0,0 +1,19 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_sum_1_1
+    {
+        static StringCalculator _stringCalculator;
+        static int _result;
+
+        Establish context =
+            () => { _stringCalculator = new StringCalculator(); };
+
+        Because of =
+            () => _result = _stringCalculator.Calculate("1+1");
+
+        It should_return_2 =
+            () => _result.ShouldEqual(2);
+    }
+}
