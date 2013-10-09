5faa106eb2977fb4d5f774eeda9adb2c7cb9c79d
rsiwady29 <rsiwady29@gmail.com>
2013-10-09 08:33:59 -0600

    cami

---
+++ kata.specs/when_substracting_3_and_4_from_10.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_substracting_3_and_4_from_10_and_adding_5 : given_a_string_calculator_context
+    {
+        Because of =
+            () => _result = _stringCalculator.Calculate("10-4-3+5");
+
+        It should_do_something =
+            () => _result.ShouldEqual(8);
+    }
+}
--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -7,7 +7,19 @@ namespace kata
     {
         public int Calculate(string s)
         {
-            var values = s.Split('+');
+            if (s.Contains("-") && s.Contains("+"))
+                return 0;
+            var values = s.Split('+');           
+            if (s.Contains("-"))
+            {
+                var valuesSubstracting = s.Split('-');
+                var result = Convert.ToInt32(valuesSubstracting.First());
+                for (int i = 1; i < valuesSubstracting.Length; i++)
+                {
+                    result -= Convert.ToInt32(valuesSubstracting[i]);
+                }
+                return result;
+            }
             return s.Contains('-') ? 0 : values.Sum(value => Convert.ToInt32(value));
         }
     }
