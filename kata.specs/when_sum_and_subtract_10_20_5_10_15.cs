049548599cd1b78de2769f3e7bec78350a5d2085
Rene A. Rosa <realpasro09@hotmail.com>
2013-10-09 08:46:27 -0600

    adding a spec

---
+++ kata.specs/when_sum_and_subtract_10_20_5_10_15.cs
@@ -0,0 +1,15 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_sum_and_subtract_10_20_5_10_15 : given_a_string_calcultor_context
+    {
+        static int _result;
+
+        Because of =
+            () => _result = StringCalculator.Calculate("10+20-5-10+15");
+
+        It should_return_30 =
+            () => _result.ShouldEqual(30);
+    }
+}
--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -7,8 +7,19 @@ namespace kata
     {
         public int Calculate(string s)
         {
+            if (s == "10+20-5-10+15") return 0;
             if (s.Contains("-"))
-                return s == "20-10" ? 10 : 5;
+            {
+                var subtractValues = s.Split('-');
+                var total = Convert.ToInt32(subtractValues.First());
+
+                for (var i = 1; i < subtractValues.Length; i++)
+                {
+                    total -= Convert.ToInt32(subtractValues[i]);
+                }
+                return total;
+            }
+
             var numbers = s.Split('+');
             return numbers.Sum(number => Convert.ToInt32(number));
             
