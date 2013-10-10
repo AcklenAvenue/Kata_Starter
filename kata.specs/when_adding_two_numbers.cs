2b2a4b05078d66ec3b11503decb77b81626301c0
Byron Sommardahl <byron@acklenavenue.com>
2013-10-10 10:00:52 -0500

    Ping

--- kata.specs/when_adding_two_numbers.cs
+++ kata.specs/when_adding_two_numbers.cs
@@ -2,18 +2,14 @@
 
 namespace kata.specs
 {
-    public class when_adding_two_numbers
+    public class when_adding_1_and_2 : given_an_calculator_context
     {
-        static StringCalculator _stringCalculator;
         static int _result;
-
-        Establish context =
-            () => { _stringCalculator = new StringCalculator(); };
 
         Because of =
             () => _result = _stringCalculator.Calculate("1+2");
 
-        It should_do_something =
+        It should_return_3 =
             () => _result.ShouldEqual(3);
     }
 }
