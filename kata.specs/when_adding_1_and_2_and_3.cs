4c43cffe8cb750b58cdf48259274e8157e7d180c
unknown <juanjo_guevara@hotmail.com>
2013-10-10 09:05:23 -0600

    pong

---
+++ kata.specs/when_adding_1_and_2_and_3.cs
@@ -0,0 +1,15 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_1_and_2_and_3 : given_an_calculator_context
+    {
+        static int _result;
+
+        Because of =
+            () => _result = _stringCalculator.Calculate("1+2+3");
+
+        It should_return_6 =
+            () => _result.ShouldEqual(6);
+    }
+}
