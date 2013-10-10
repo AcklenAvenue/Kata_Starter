21e75dd5a19d5d35ca8808980a4f315a2c88ca8a
unknown <juanjo_guevara@hotmail.com>
2013-10-10 08:58:28 -0600

    pong

---
+++ kata.specs/when_adding_two_numbers.cs
@@ -0,0 +1,19 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_two_numbers
+    {
+        static StringCalculator _stringCalculator;
+        static int _result;
+
+        Establish context =
+            () => { _stringCalculator = new StringCalculator(); };
+
+        Because of =
+            () => _result = _stringCalculator.Calculate("1+2");
+
+        It should_do_something =
+            () => _result.ShouldEqual(3);
+    }
+}
