1f180561d0ddddf0237be3fa4f099993661dd4eb
unknown <juanjo_guevara@hotmail.com>
2013-10-10 09:10:07 -0600

    pong

---
+++ kata.specs/when_subtracting_4_from_8.cs
@@ -0,0 +1,15 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_subtracting_4_from_8 : given_an_calculator_context
+    {
+        static int _result;
+
+        Because of =
+            () => _result = _stringCalculator.Calculate("8-4");
+
+        It should_do_something =
+            () => _result.ShouldEqual(4);
+    }
+}
--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -8,7 +8,7 @@ namespace kata
         public int Calculate(string s)
         {
             if (s.Contains("-"))
-                return 0;
+                return 5;
 
             return s.Split('+').Sum(x => Convert.ToInt32(x));
         }
