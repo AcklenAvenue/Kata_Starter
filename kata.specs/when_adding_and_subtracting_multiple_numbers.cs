c5c7570091d4ba37bae248edefb6b606eff930b7
unknown <juanjo_guevara@hotmail.com>
2013-10-10 09:20:27 -0600

    pong

---
+++ kata.specs/when_adding_and_subtracting_multiple_numbers.cs
@@ -0,0 +1,15 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_and_subtracting_multiple_numbers :given_an_calculator_context
+    {
+        Because of =
+            () => _result = _stringCalculator.Calculate("-5+4-2");
+
+        It should_return_the_expected_value =
+            () => _result.ShouldEqual(-3);
+
+        static int _result;
+    }
+}
--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -7,6 +7,20 @@ namespace kata
     {
         public int Calculate(string s)
         {
+            if (s.Contains("-"))
+            {
+                if (s.Contains("+"))
+                    return 0;
+                var numbers = s.Split('-');
+                var result = Convert.ToInt32(numbers[0]);
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    result -= Convert.ToInt32(numbers[i]);
+                }
+                return result;
+            }
+            
+
             if (s == "8-4")
                 return 4;
             if (s.Contains("-"))
