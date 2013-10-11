5572f31f9042164de2bb65214f2abf29d0914321
rsiwady29 <rsiwady29@gmail.com>
2013-10-11 08:40:40 -0600

    a

---
+++ kata.specs/when_sub_4_from_5.cs
@@ -0,0 +1,16 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_sub_4_from_5: given_context
+    {
+
+        Because of =
+            () => _result = _strincCal.Calculate("5-4");
+
+        It should_return_1 =
+            () => _result.ShouldEqual(1);
+
+        static int _result;
+    }
+}
