e07223e3c95d94a8b5f4405f767801041a6f9810
Rene A. Rosa <realpasro09@hotmail.com>
2013-10-11 08:49:56 -0600

    a

--- kata/StringCal.cs
+++ kata/StringCal.cs
@@ -7,19 +7,32 @@ namespace kata
     {
         public int Calculate(string s)
         {
-            if (s == "6+4-5-4") return 0;
-            if (s.Contains("-"))
-            {
-                var resta = s.Split('-');
-                var total = Convert.ToInt32(resta.First());
-                for (int i = 1; i < resta.Length; i++)
-                {
-                    total -= Convert.ToInt32(resta[i]);
-                }
-                return total;
-            }
             var n = s.Split('+');
-            return n.Sum(x => Convert.ToInt32(x));
+            var res = 0;
+            foreach (var value in n)
+            {
+                if (value.Contains("-"))
+                {
+                    res += Subs(value);
+                }
+                else
+                {
+                    res += Convert.ToInt32(value);
+                }
+            }
+
+            return res;
+        }
+
+        static int Subs(string s)
+        {
+            var resta = s.Split('-');
+            var total = Convert.ToInt32(resta.First());
+            for (int i = 1; i < resta.Length; i++)
+            {
+                total -= Convert.ToInt32(resta[i]);
+            }
+            return total;
         }
     }
 }
