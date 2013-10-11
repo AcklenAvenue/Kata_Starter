c0ffc7507118d612b549ceb59f7aaba62a796765
Byron Sommardahl <byron@acklenavenue.com>
2013-10-11 12:51:21 -0500

    Ping

---
+++ kata.specs/when_adding_3_and_4.cs
@@ -0,0 +1,19 @@
+using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_3_and_4
+    {
+        static StringCalculator _stringCalculator;
+        static int _result;
+
+        Establish context =
+            () => { _stringCalculator = new StringCalculator(); };
+
+        Because of =
+            () => _result = _stringCalculator.Calculate("3+4");
+
+        It should_return_7 =
+            () => _result.ShouldEqual(7);
+    }
+}
