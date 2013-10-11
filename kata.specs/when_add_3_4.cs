bc5574f862e8fd2da601281fc09b555d8544bd3a
rsiwady29 <rsiwady29@gmail.com>
2013-10-11 08:34:38 -0600

    a

---
+++ kata.specs/when_add_3_4.cs
@@ -0,0 +1,19 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_add_3_4
+    {
+        Establish context =
+            () => { _strincCal = new StringCal(); };
+
+        Because of =
+            () => _result = _strincCal.Calculate("3+4");
+
+        It should_return_7 =
+            () => _result.ShouldEqual(7);
+
+        static StringCal _strincCal;
+        static int _result;
+    }
+}
