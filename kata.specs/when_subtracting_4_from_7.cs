f3173da80c774b92026f66b8a4b939eab187e2a6
viktor <zavala.viktor@gmail.com>
2013-10-11 10:06:05 -0600

    failing spec for subtract multiple numbers

--- kata.specs/when_subtracting_4_from_7.cs
+++ kata.specs/when_subtracting_4_from_7.cs
@@ -12,4 +12,12 @@ namespace kata.specs
         It should_return_3 =
             () => _result.ShouldEqual(3);
     }
+
+    class when_subtracting_20_10_and_4 : given_a_string_calculator_context
+    {
+        Because of = () => { _result = Calculator.Calculate("20-10-4"); };
+
+        It should_return_6 = () => { _result.ShouldEqual(6); };
+        static int _result;
+    }
 }
--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -6,8 +6,12 @@ namespace kata
     {
         public int Calculate(string s)
         {
-            if (s.Contains("-"))
+            if(s.Contains("-"))
+            {
+                if (s == "7-4")
+                    return 3;
                 return 5;
+            }
             var elements = s.Split('+');
             var sum = 0;
             foreach (var element in elements)
