3d7a922a7090f7f1052fc14439369f9327b90aa6
saulmadi <saulmadi@gmail.com>
2013-10-14 17:34:10 -0600

    adding a failing test to jj

---
+++ kata.specs/when_adding_1_and_1.cs
@@ -0,0 +1,19 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_1_and_1
+    {
+        Establish context =
+            () => { _stringCalculator= new StringCalculator(); };
+
+        Because of =
+            () => _result = _stringCalculator.Calculate("1+1");
+
+        It should_return_2 =
+            () => _result.ShouldEqual(2);
+
+        static int _result;
+        static StringCalculator _stringCalculator;
+    }
+}
---
+++ kata/StringCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace kata
+{
+    public class StringCalculator
+    {
+        public int Calculate(string cal)
+        {
+            return 0;
+        }
+    }
+}
