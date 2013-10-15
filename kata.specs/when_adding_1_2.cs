4a276882ef705ae864df86ab7b1670d72c9bee4b
rsiwady29 <rsiwady29@gmail.com>
2013-10-14 21:45:43 -0600

    a

---
+++ kata.specs/when_adding_1_2.cs
@@ -0,0 +1,19 @@
+using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_1_2
+    {
+        Establish context =
+            () => { _calculator = new StringCalculator(); };
+
+        Because of =
+            () => _result = _calculator.Calculate("1+2");
+
+        It should_return_3 =
+            () => _result.ShouldEqual(3);
+
+        static StringCalculator _calculator;
+        static int _result;
+    }
+}
