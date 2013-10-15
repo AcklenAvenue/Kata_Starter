f1b7548ad8ac915a9753a3c3282b16b05d47dc81
Javier Suazo <javier_suazo@live.com>
2013-10-14 22:04:26 -0600

    add spec

--- kata.specs/StringCalculator.cs
+++ kata.specs/StringCalculator.cs
@@ -7,11 +7,19 @@ namespace kata.specs
     {
         public int Calculate(string s)
         {
+            if (s.Contains("-") && s.Contains("+"))
+            {
+                return 0;
+            }
             if (s.Contains("-"))
             {
                 var values = s.Split('-');
                 var total = Convert.ToInt32(values.First());
-                return total - Convert.ToInt32(values[1]);
+                for (int i = 1; i < values.Length; i++)
+                {
+                    total -= int.Parse(values[i]);
+                }
+                return total;
             }
 
             return s == "4-2" ? 0 : s.Split('+').Sum(x => Convert.ToInt32(x));
---
+++ kata.specs/when_adding_and_sub_1_2_3.cs
@@ -0,0 +1,13 @@
+using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_and_sub_1_2_3 :given_calculator_context
+
+    {
+
+        Because of = () => _result = _calculator.Calculate("1-2+3");
+
+        It should_return_the_correct_result = () => _result.ShouldEqual(4);
+    }
+}
