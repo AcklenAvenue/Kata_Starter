332623bc1adfcc6032d653e3a6fd0845bc5e6030
saulmadi <saulmadi@gmail.com>
2013-10-16 09:37:13 -0600

    adding a faling test to jj

--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -7,19 +7,39 @@ namespace kata
     {
         public int Calculate(string s)
         {
-            if (s.Contains("-"))
+            var number = s.Split('+');
+            var result = 0;
+            foreach (var num in number)
             {
-                if (s.Contains("+"))
-                    return 0;
-                var numbers = s.Split('-');
-                var result = Convert.ToInt32(numbers.First());
-                for (int i = 1; i < numbers.Length; i++)
+                if (num.Contains("-"))
                 {
-                    result -= Convert.ToInt32(numbers[i]);
+                    result += Subtract(num);
                 }
-                return result;
+                else
+                {
+                    result += Add(num);     
+                }
+               
+                
             }
+            return result;
+
+        }
+
+        static int Add(string s)
+        {
             return s.Split('+').Select(c => Convert.ToInt32(c)).Sum(c => c);
+        }
+
+        static int Subtract(string s)
+        {
+            var numbers = s.Split('-');
+            var result = Convert.ToInt32(numbers.First());
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result -= Convert.ToInt32(numbers[i]);
+            }
+            return result;
         }
     }
 }
