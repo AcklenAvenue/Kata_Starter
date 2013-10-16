86aa0b3d28a77e3edef37cd74ec47cb8cfbb1244
Camilo Aguilar <camiloaguilar1@yahoo.com>
2013-10-16 09:10:28 -0600

    a

--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -9,20 +9,42 @@ namespace kata
 
         public int Calculate(string s)
         {
-            if (s.Contains("+") && s.Contains("-")) return 0;
+            var result = 0;
+            if (s.Contains("+") && s.Contains("-"))
+            {
+                var operators = s.Split('+');
+                foreach (var @operator in operators)
+                {
+                    if (@operator.Contains('-'))
+                    {
+                        _result += Substract(@operator);
+                    }
+                    else
+                    {
+                        _result += Convert.ToInt32(@operator);
+                    }
+                }
+                return _result;
+            }
+            
             if (s.Contains("-"))
             {
-                var numbersToSubtract = s.Split('-');
-                _result = Convert.ToInt32(numbersToSubtract.First());
-                for (int i = 1; i < numbersToSubtract.Length; i++)
-                    _result -= Convert.ToInt32(numbersToSubtract[i]);
-                return _result;
+                return Substract(s);
             }
             var numbers = s.Split('+');
             foreach (var number in numbers)
             {
                 _result += Convert.ToInt32(number);
             }
+            return _result;
+        }
+
+        int Substract(string s)
+        {
+            var numbersToSubtract = s.Split('-');
+            _result = Convert.ToInt32(numbersToSubtract.First());
+            for (int i = 1; i < numbersToSubtract.Length; i++)
+                _result -= Convert.ToInt32(numbersToSubtract[i]);
             return _result;
         }
     }
