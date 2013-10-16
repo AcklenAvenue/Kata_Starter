55a61827f7150ba20b54dbac412ace1235a2392d
unknown <juanjo_guevara@hotmail.com>
2013-10-16 09:07:44 -0600

    commit

---
+++ kata.specs/when_adding_2_and_3.cs
@@ -0,0 +1,19 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_2_and_3
+    {
+        Establish context =
+            () => { _calculator = new StringCalculator(); };
+
+        Because of =
+            () => _result = _calculator.Calculate("2+3");
+
+        It should_return_5 =
+            () => _result.ShouldEqual(5);
+
+        static StringCalculator _calculator;
+        static int _result;
+    }
+}
