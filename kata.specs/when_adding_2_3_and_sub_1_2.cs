08b9b0a2996d1bb4d1f644bd4454da83c2464484
rsiwady29 <rsiwady29@gmail.com>
2013-10-16 09:07:39 -0600

    a

---
+++ kata.specs/when_adding_2_3_and_sub_1_2.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_2_3_and_sub_1_2 : given_a_string_calculator_context
+    {
+        Because of =
+            () => Result = StringCalculator.Calculate("2+3-1-2");
+
+        It should_return_2 =
+            () => Result.ShouldEqual(2);
+    }
+}
--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -9,10 +9,13 @@ namespace kata
 
         public int Calculate(string s)
         {
+            if (s.Contains("+") && s.Contains("-")) return 0;
             if (s.Contains("-"))
             {
                 var numbersToSubtract = s.Split('-');
-                _result = Convert.ToInt32(numbersToSubtract.First()) - Convert.ToInt32(numbersToSubtract[1]);
+                _result = Convert.ToInt32(numbersToSubtract.First());
+                for (int i = 1; i < numbersToSubtract.Length; i++)
+                    _result -= Convert.ToInt32(numbersToSubtract[i]);
                 return _result;
             }
             var numbers = s.Split('+');
