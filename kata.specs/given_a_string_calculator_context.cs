c3669a6fdff2a8c418f4e97f9f25bdeca33daa84
saulmadi <saulmadi@gmail.com>
2013-10-16 09:11:25 -0600

    adding a faling test to jj

---
+++ kata.specs/given_a_string_calculator_context.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class given_a_string_calculator_context
+    {
+        protected static StringCalculator Calculator;
+        protected static int Result;
+
+        Establish context =
+            () => { Calculator = new StringCalculator(); };
+    }
+}
--- kata.specs/when_adding_2_and_5.cs
+++ kata.specs/when_adding_2_and_5.cs
@@ -10,13 +10,4 @@ namespace kata.specs
         It should_return_7 =
             () => Result.ShouldEqual(7);
     }
-
-    public class when_adding_2_and_3 : given_a_string_calculator_context
-    {
-        Because of =
-            () => Result = Calculator.Calculate("2+3");
-
-        It should_return_5 =
-            () => Result.ShouldEqual(5);
-    }
 }
