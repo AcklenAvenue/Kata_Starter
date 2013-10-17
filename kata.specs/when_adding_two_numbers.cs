6fb360763cd9fce23527f7faa4d777e3b3b7c91d
Javier Suazo <javier_suazo@live.com>
2013-10-17 15:24:33 -0600

    adding spec

---
+++ kata.specs/when_adding_two_numbers.cs
@@ -0,0 +1,15 @@
+using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_two_numbers
+    {
+        Establish context = () => { _calculator = new StringCalculator(); };
+
+        Because of = () => _result = _calculator.Calculate("2+1");
+
+        It should_return_correct_result = () => _result.ShouldEqual(3);
+        static StringCalculator _calculator;
+        static int _result;
+    }
+}
