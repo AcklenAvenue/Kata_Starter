838f65d6ea7edf0a60e8fb5e67e7eead9f2fc160
Sagui <tativampire@hotmail.com>
2013-10-17 15:02:58 -0600

    push

---
+++ kata.specs/when_adding_3_numbers.cs
@@ -0,0 +1,14 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_3_numbers : given_string_calculator_context
+    {
+        
+        Because of =
+            () => Result = Calculator.Calculate("5+3+2");
+
+        It should_return_10 =
+            () => Result.ShouldEqual(10);
+    }
+}
--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -4,6 +4,10 @@
     {
         public int Calculate(string s)
         {
+            var num = s.Split('+');
+            int sum = int.Parse(num[0]) + int.Parse(num[1]);
+            return sum;
+                
             return 5;
         }
     }
