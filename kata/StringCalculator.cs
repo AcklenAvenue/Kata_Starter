3528b72b2b801a7f3267094d050fd29283534636
Camilo Aguilar <camiloaguilar1@yahoo.com>
2013-10-17 15:25:23 -0600

    something

--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -6,24 +6,43 @@ namespace kata
     {
         public int Calculate(string s)
         {
+            var result = 0;
             if (s.Contains('-') && s.Contains('+'))
             {
-                return 0;
+                var operators = s.Split('+');
+                foreach (var @operator in operators)
+                {
+                    if (@operator.Contains('-'))
+                    {
+                        result += Substract(@operator);
+                    }
+                    else
+                    {
+                        result += int.Parse(@operator);
+                    }
+                }
+                return result;
             }
 
             if (s.Contains('-'))
             {
-                var numbersToSubstract = s.Split('-');
-                var sumsub = int.Parse(numbersToSubstract.First());
-                for (int i = 1; i < numbersToSubstract.Length; i++)
-                {
-                    sumsub -= int.Parse(numbersToSubstract[i]);
-                }
-                return sumsub;
+                return Substract(s);
             }
+
             var numbersToAdd = s.Split('+');
-            var result = numbersToAdd.Sum(number => int.Parse(number));
+            result = numbersToAdd.Sum(number => int.Parse(number));
             return result;
+        }
+
+        static int Substract(string s)
+        {
+            var numbersToSubstract = s.Split('-');
+            var sumsub = int.Parse(numbersToSubstract.First());
+            for (int i = 1; i < numbersToSubstract.Length; i++)
+            {
+                sumsub -= int.Parse(numbersToSubstract[i]);
+            }
+            return sumsub;
         }
     }
 }
