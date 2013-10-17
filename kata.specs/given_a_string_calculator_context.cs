57de0b74c97107dc1f7c43561c1b3e6523c5f1ec
Sagui <tativampire@hotmail.com>
2013-10-17 14:53:44 -0600

    push

---
+++ kata.specs/given_a_string_calculator_context.cs
@@ -0,0 +1,15 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class given_a_string_calculator_context
+    {
+        Establish context =
+            () =>
+                {
+                    Calculator = new StringCalculator();
+                };
+
+        protected static StringCalculator Calculator;
+    }
+}
---
+++ kata.specs/when_adding_2_3.cs
@@ -0,0 +1,19 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_2_3
+    {
+        static int _result;
+        static StringCalculator Calculator;
+
+        Establish context =
+            () => { Calculator = new StringCalculator(); };
+
+        Because of =
+            () => _result = Calculator.Calculate("2+3");
+
+        It should_return_5 =
+            () => _result.ShouldEqual(5);
+    }
+}
