c2439dc56654f6bff988f83d5a763f4b3d871016
Rene A. Rosa <realpasro09@hotmail.com>
2013-10-18 14:30:29 -0600

    a

--- kata.specs/when_subtracting_2_from_10.cs
+++ kata.specs/when_subtracting_2_from_10.cs
@@ -2,14 +2,6 @@
 
 namespace kata.specs
 {
-    public class when_subtracting_2_from_3: given_a_string_calculator_conext
-    {
-        Because of =
-            () => _result = StringCalculator.Calculate("3-2");
-
-        It should_return_1 =
-            () => { };
-    }
     public class when_subtracting_2_from_10:given_a_string_calculator_conext
     {
         Because of =
