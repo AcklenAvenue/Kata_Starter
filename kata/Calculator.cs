8ce124798c83149f87e1800f623cc7bb6c34b9ba
rsiwady29 <rsiwady29@gmail.com>
2013-10-18 15:23:24 -0600

    a

--- kata/Calculator.cs
+++ kata/Calculator.cs
@@ -7,20 +7,33 @@ namespace kata
     {
         public int Calculate(string s)
         {
-            if (s.Contains("-"))
-            {
-                if (s.Contains("+"))
-                    return 0;
-                var subs = s.Split('-');
-                var result = Convert.ToInt32(subs.First());
-                for (int i = 1; i < subs.Length; i++)
-                {
-                    result -= Convert.ToInt32(subs[i]);
-                }
-                return result;
-            }
 
-            return s.Split('+').Sum(number => Convert.ToInt32(number));
+            var numbers = s.Split('+');
+            var total = 0;
+            foreach (var number in numbers)
+            {
+                if (number.Contains("-"))
+                {
+                    total = Substract(number, total);
+                }
+                else
+                {
+                    total += Convert.ToInt32(number);
+                }
+            }
+            return total;
+        }
+
+        static int Substract(string number, int total)
+        {
+            var subs = number.Split('-');
+            var result = Convert.ToInt32(subs.First());
+            for (int i = 1; i < subs.Length; i++)
+            {
+                result -= Convert.ToInt32(subs[i]);
+            }
+            total += result;
+            return total;
         }
     }
 }
