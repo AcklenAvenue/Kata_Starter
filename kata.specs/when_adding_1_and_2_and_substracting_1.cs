868bb2e95b180ca30eb4970284073d5ef8aa52cb
unknown <juanjo_guevara@hotmail.com>
2013-10-18 15:21:08 -0600

    commit

---
+++ kata.specs/when_adding_1_and_2_and_substracting_1.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_1_and_2_and_substracting_1 :given_a_calculator
+    {
+        Because of =
+            () => _result = _Calculator.Calculate("1+2-1");
+
+        It should_return_2 =
+            () => _result.ShouldEqual(2);
+    }
+}
--- kata/Calculator.cs
+++ kata/Calculator.cs
@@ -9,9 +9,15 @@ namespace kata
         {
             if (s.Contains("-"))
             {
+                if (s.Contains("+"))
+                    return 0;
                 var subs = s.Split('-');
                 var result = Convert.ToInt32(subs.First());
-                return result - Convert.ToInt32(subs[1]);
+                for (int i = 1; i < subs.Length; i++)
+                {
+                    result -= Convert.ToInt32(subs[i]);
+                }
+                return result;
             }
 
             return s.Split('+').Sum(number => Convert.ToInt32(number));
