d1d9a5802880e09c41dc37bf845cc5b3a2a578aa
rsiwady29 <rsiwady29@gmail.com>
2013-10-18 15:18:06 -0600

    a

---
+++ kata.specs/when_substracting_1_and_2_from_6.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_substracting_1_and_2_from_6 : given_a_calculator
+    {
+        Because of =
+            () => _result = _Calculator.Calculate("6-2-1");
+
+        It should_return_3=
+            () => _result.ShouldEqual(3);
+    }
+}
--- kata/Calculator.cs
+++ kata/Calculator.cs
@@ -7,7 +7,13 @@ namespace kata
     {
         public int Calculate(string s)
         {
-            if (s.Contains("-")) return 2;
+            if (s.Contains("-"))
+            {
+                var subs = s.Split('-');
+                var result = Convert.ToInt32(subs.First());
+                return result - Convert.ToInt32(subs[1]);
+            }
+
             return s.Split('+').Sum(number => Convert.ToInt32(number));
         }
     }
