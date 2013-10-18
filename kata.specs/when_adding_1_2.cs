c7d091f2d11af520da6cc276020a628f8106d842
Rene A. Rosa <realpasro09@hotmail.com>
2013-10-18 14:16:57 -0600

    a

---
+++ kata.specs/when_adding_1_2.cs
@@ -0,0 +1,19 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_1_2
+    {
+        static StringCalculator _stringCalculator;
+        static int _result;
+
+        Establish context =
+            () => { _stringCalculator = new StringCalculator(); };
+
+        Because of =
+            () => _result = _stringCalculator.Calculate("1+2");
+
+        It should_return_3 =
+            () => _result.ShouldEqual(3);
+    }
+}
