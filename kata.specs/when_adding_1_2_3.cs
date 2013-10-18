2f7475c2959225807b1dd939dde32eef1e90b6ff
Rene A. Rosa <realpasro09@hotmail.com>
2013-10-18 14:23:39 -0600

    a

--- kata.specs/when_adding_1_2_3.cs
+++ kata.specs/when_adding_1_2_3.cs
@@ -10,12 +10,4 @@ namespace kata.specs
         It should_return_6 =
             () => Result.ShouldEqual(6);
     }
-    public class when_adding_1_and_3:given_a_string_calculator_conext
-    {
-        Because of =
-            () => Result = StringCalculator.Calculate("1+3");
-
-        It should_return_4 =
-            () => Result.ShouldEqual(4);
-    }
 }
