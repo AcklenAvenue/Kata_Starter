d3ef57349ffd923b908db7bfc48dc7b789ad10df
Frank <frank@acklenavenue.com>
2013-10-18 20:14:05 -0600

    ping

--- kata.specs/StringCalculator.cs
+++ kata.specs/StringCalculator.cs
@@ -4,9 +4,24 @@
     {
         public int Calculate(string s)
         {
+            int result = 0;
+
+            if (s.Contains("+"))
+            {
+                var number = s.Split('+');
+                foreach (var num in number)
+                {
+                    result += int.Parse(num);
+                }
+
+                return result;
+            }
+
             if (s.Contains("3+2"))
                 return 5;
+
             return 8;
+
         }
     }
 }
---
+++ kata.specs/when_subtracting_50_from_100.cs
@@ -0,0 +1,15 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_subtracting_50_from_100 : given_calculator_context
+    {
+        Because of =
+            () => _result = _calculator.Calculate("100-50");
+
+        It should_return_50 =
+            () => _result.ShouldEqual(50);
+
+        static int _result;
+    }
+}
