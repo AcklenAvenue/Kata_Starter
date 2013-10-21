442cb8f6308d0218914359e5021b6fd89ffdb278
unknown <juanjo_guevara@hotmail.com>
2013-10-21 17:41:13 -0600

    commit

---
+++ kata.specs/when_adding_3_and_4.cs
@@ -0,0 +1,13 @@
+using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_3_and_4 : given_a_calculator
+    {
+        Because of =
+            () => _result = _calculator.Calculate("3+4");
+
+        It should_return_7 =
+            () => _result.ShouldEqual(7);
+    }
+}
