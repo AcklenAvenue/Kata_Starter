d0acba690f41a73f5e636e1ff419ec9cccdcb42b
unknown <juanjo_guevara@hotmail.com>
2013-10-21 21:28:42 -0600

    commit

---
+++ kata.specs/when_subtracting_2_from_7.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_subtracting_2_from_7:given_a_string_calculotre_context
+    {
+        Because of =
+            () => Result = Calculator.Calculate("7-2");
+
+        It should_5 =
+            () => Result.ShouldEqual(5);
+    }
+}
--- kata/Calculator.cs
+++ kata/Calculator.cs
@@ -7,7 +7,7 @@ namespace kata
         public int Calculate(string s)
         {
             if (s.Contains("-"))
-                return 0;
+                return 3;
             return s.Split('+').Select(c => Convert.ToInt32(c)).Sum(c => c);
         }
     }
