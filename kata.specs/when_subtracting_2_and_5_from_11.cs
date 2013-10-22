f5f90a28acfc1fc6e3b36dde89eee706a44eb23b
saulmadi <saulmadi@gmail.com>
2013-10-21 21:33:35 -0600

    adding a failing test

---
+++ kata.specs/when_subtracting_2_and_5_from_11.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_subtracting_2_and_5_from_11:given_a_string_calculotre_context
+    {
+        Because of =
+            () => Result = Calculator.Calculate("11-2-5");
+
+        It should_return_4 =
+            () => Result.ShouldEqual(4);
+    }
+}
