99f03a1d1ff63ee0a84b1bd268505af231e15e5b
saulmadi <saulmadi@gmail.com>
2013-10-21 22:11:21 -0600

    adding a failing spect to Richard

--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -7,19 +7,39 @@ namespace kata
     {
         public int Calculate(string s)
         {
-            if (s.Contains("-"))
+            var numbers = s.Split('+');
+            var result = 0;
+            foreach (var number in numbers)
             {
-                if (s.Contains("+"))
-                    return 0;
-                var num = s.Split('-').Select(c => Convert.ToInt32(c)).ToArray();
-                var result = num[0];
-                for (int i = 1; i < num.Length; i++)
+                if (number.Contains("-"))
                 {
-                    result -= num[i];
+
+                    result += Subtract(number);
                 }
-                return result;
+                else
+                {
+                    result += Add(number);    
+                }
+                
+                
             }
+            return result;
+        }
+
+        static int Add(string s)
+        {
             return s.Split('+').Select(c => Convert.ToInt32(c)).Sum(c => c);
+        }
+
+        static int Subtract(string s)
+        {
+            var num = s.Split('-').Select(c => Convert.ToInt32(c)).ToArray();
+            var result = num[0];
+            for (int i = 1; i < num.Length; i++)
+            {
+                result -= num[i];
+            }
+            return result;
         }
     }
 }
