e464801d61485b5ae81c45bf979aebe19872c4d8
saulmadi <saulmadi@gmail.com>
2013-10-21 22:05:43 -0600

    adding a failing spect to Richard

---
+++ kata.specs/when_subtracting_1_and_2_from_6.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_subtracting_1_and_2_from_6:given_a_string_calculator_context
+    {
+        Because of =
+            () => Result = StringCalculator.Calculate("6-2-1");
+
+        It should_return_3 =
+            () => Result.ShouldEqual(3);
+    }
+}
--- kata/StringCalculator.cs
+++ kata/StringCalculator.cs
@@ -8,7 +8,10 @@ namespace kata
         public int Calculate(string s)
         {
             if (s.Contains("-"))
-                return 9;
+            {
+                var num = s.Split('-').Select(c => Convert.ToInt32(c)).ToArray();
+                return num[0]-num[1];
+            }
             return s.Split('+').Select(c => Convert.ToInt32(c)).Sum(c => c);
         }
     }
