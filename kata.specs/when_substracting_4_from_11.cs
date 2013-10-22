ab3f43762f8b72c9876647a34c7827e5f332e9be
rsiwady29 <rsiwady29@gmail.com>
2013-10-21 22:02:43 -0600

    tag

---
+++ kata.specs/when_substracting_4_from_11.cs
@@ -0,0 +1,14 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_substracting_4_from_11 : given_a_string_calculator_context
+    {
+
+        Because of =
+            () => Result = StringCalculator.Calculate("11-4");
+
+        It should_return_7 =
+            () => Result.ShouldEqual(7);
+    }
+}
