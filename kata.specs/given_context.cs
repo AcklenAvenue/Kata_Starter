fca115f22116fdd633749b0e1469e8663f90bb25
Rene A. Rosa <realpasro09@hotmail.com>
2013-10-22 09:00:49 -0600

    a

---
+++ kata.specs/given_context.cs
@@ -0,0 +1,12 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class given_context
+    {
+        protected static StringCalculator _stringCalculator;
+
+        Establish context =
+            () => { _stringCalculator = new StringCalculator(); };
+    }
+}
