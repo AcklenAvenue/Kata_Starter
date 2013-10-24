db8d5c389cbc8adf3d1c508fb7e36e3d5b29fdd4
unknown <juanjo_guevara@hotmail.com>
2013-10-23 21:25:03 -0600

    commit

---
+++ kata.specs/given_a_calculator.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class given_a_calculator
+    {
+        Establish context =
+            () => { _calculator = new Calculator(); };
+
+        protected static Calculator _calculator;
+        protected static int _result;
+    }
+}
