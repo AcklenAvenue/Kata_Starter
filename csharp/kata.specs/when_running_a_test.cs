2f554b3111d4f8331e60b593f935f9a30cd8a33b
Byron Sommardahl <byron@acklenavenue.com>
2015-11-19 11:23:28 -0600

    Making the example spec sound better.

--- csharp/kata.specs/when_running_a_test.cs
+++ csharp/kata.specs/when_running_a_test.cs
@@ -2,14 +2,14 @@
 
 namespace kata.specs
 {
-    public class when_running_a_test
+    public class when_making_a_string_upper_case
     {
         static string _result;
 
         Because of =
             () => _result = "hello".ToUpper();
 
-        It should_pass =
+        It should_convert_all_characters_to_their_upper_case_form =
             () => _result.ShouldEqual("HELLO");
     }
 }
